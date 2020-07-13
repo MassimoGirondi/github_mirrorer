d78902d605c7da5fca3a90ab97ae4351ebcfe68b
Massimo Girondi <massimo@girondi.net>
2020-07-13 20:51:55 +0200

    Reorganized entrypoint; gitweb on port 8080 without any authenticathion

---
+++ docker_files/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /
+./get_repos.py
+if [ $UPDATE_ON_START != 0 ]; then
+	./mirror_repos.sh
+fi
+cron
+
+cd /repos
+cd `find -iname HEAD | head -n 1 | sed -e 's/HEAD//'`
+git instaweb --port=80 
+
+tail -f /var/log/cron.log
+
